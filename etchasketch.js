29e594bc02d7081edcf9bf9d94520a58c28b1540
James S <98997059+jamesfsher@users.noreply.github.com>
2023-08-05 19:42:56 -0500

    fixed container

--- etchasketch.js
+++ etchasketch.js
@@ -4,12 +4,17 @@
 
 function createGrid() {
     const num = 16;
+    const dim = 960/num + 'px';
     for (i = 0; i < num; i++) {
-        const newDiv = document.createElement("div");
-        newDiv.style.width = '100px';
-        newDiv.style.height = '100px';
-        newDiv.style.background = 'red';
-        document.querySelector(".containerGrid").appendChild(newDiv);
+        for (j = 0; j < num; j++) {
+            const newDiv = document.createElement("div");
+            newDiv.style.width = dim;
+            newDiv.style.height = dim;
+            // newDiv.style.borderStyle = 'solid';
+            newDiv.style.backgroundColor = 'lightblue';
+            newDiv.style.flex = '0 1 auto';
+            document.querySelector(".containerGrid").appendChild(newDiv);    
+        }
     }
 }
 
