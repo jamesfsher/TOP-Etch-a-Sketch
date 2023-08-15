7badd94eb631c3a6c8caf56b2ebfef4d8d95b3ee
James S <98997059+jamesfsher@users.noreply.github.com>
2023-08-14 21:37:03 -0500

    prompt button working

--- etchasketch.js
+++ etchasketch.js
@@ -3,12 +3,16 @@
 // place them in another container
 
 // Function to create drawing grid
-function createGrid() {
-    // Variable to set number of pixels wide/height
-    const num = 30;
-    // Ensures width/height isn't greater than 50 as to not take up computational power
-    if (num > 50) {
-        num = 50;
+function createGrid(num) {
+    // Clear etch a sketch container
+    document.querySelector(".containerGrid").innerHTML = "";
+    // If no user selection, default value will be 32 pixels
+    if (!num) {
+        num = 32;
+    }
+    // Ensures width/height isn't greater than 100 as to not take up computational power
+    if (num > 100) {
+        num = 100;
     }
     // Creates new variable to set height and width
     const dim = 960/num + 'px';
@@ -32,6 +36,23 @@ function createGrid() {
 function draw(event) {
     event.currentTarget.style.backgroundColor = 'black';
 }
+
+// Function to allow user to change grid size
+function changeGrid() {
+    let size = prompt("Enter new grid size");
+    size = size.trim();
+    if (size == null) {
+        console.log("null");
+    }
+    // Ensures user only enters a number for grid size
+    while (isNaN(size) || size == null || !size) {
+        size = prompt("Enter a number only");
+        size = size.trim();
+
+    }
+    createGrid(size);
+}
+
 
 // Call function to create the pixel Grid
 createGrid();
